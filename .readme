ccec0817f2dec93db920f1eb4a727e3069c799bc
alessandra martera <ale.martera@gmail.com>
2023-11-07 15:20:20 +0100

    update read me with target

---
+++ .readme
@@ -0,0 +1,2 @@
+Creiamo un array di frasi ispirazionali e creiamo una funzione che ritornerà ogni volta una frase random dall’array.
+Il server dovrà rispondere invocando la funzione e stampando la frase random dentro un tag H1.
--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+// importo la libreria dotenv
+const dotenv = require("dotenv");
 
+// la avvio
+dotenv.config();
 
+// funzione per un numero casuale con la quale estrarre una frase casuale in poszione randomNum 
 function randomString(){
 const arrayString = [ 
     "non dire gatto se non lo hai nel sacco", 
-    "il mattino a l'oro in bocca", 
-    "Non c/’è peggior sordo di chi, in effetti, non sente", 
+    "il mattino ha l'oro in bocca", 
+    "Non c'e' peggior sordo di chi, in effetti, non sente", 
     "Chi va con lo zoppo va piano piano", 
-    "Il lupo perde il pelo e diventa calvo"
+    "Il lupo perde il pelo e diventa calvo",
+    "Chi lascia la strada vecchia per la nuova arriva prima perchè è asfaltata",
 ]
-const randomNum = Math.floor( Math.random() * ( arrayString.length - 0) ) + 0;    
+
+
+const randomNum = Math.floor( Math.random() * ( arrayString.length - 0) ) + 0; 
+
+if()
 let bob = arrayString[randomNum];
     return bob;
 }
@@ -25,7 +35,7 @@ const server = http.createServer(function (req, res) {
     res.writeHead(200, { 
         "Content-Type": "text/html"
         });
-    res.end(`<h1> ${randomString()} </h1>`)
+    res.end(`<h1 style="text-aling:center"> ${randomString()} </h1>`)
     // console.log("http\\localhost:" + process.env.PORT)
   }).listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`
