c2adefb4eb2b80ab091c439d3b752e93990b05fb
alessandra martera <ale.martera@gmail.com>
2023-11-07 15:11:06 +0100

    function random string

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
+
+
+function randomString(){
+const arrayString = [ 
+    "non dire gatto se non lo hai nel sacco", 
+    "il mattino a l'oro in bocca", 
+    "Non c/’è peggior sordo di chi, in effetti, non sente", 
+    "Chi va con lo zoppo va piano piano", 
+    "Il lupo perde il pelo e diventa calvo"
+]
+const randomNum = Math.floor( Math.random() * ( arrayString.length - 0) ) + 0;    
+let bob = arrayString[randomNum];
+    return bob;
+}
+
 // let port= process.env.PORT || 3000;
 const header={}
 const server = http.createServer(function (req, res) {
-    res.end("hello world okay ci siamo")
+    res.writeHead(200, { 
+        "Content-Type": "text/html"
+        });
+    res.end(`<h1> ${randomString()} </h1>`)
     // console.log("http\\localhost:" + process.env.PORT)
   }).listen(port, host, () => {
     const serverUrl = `http://${host}:${port}`
